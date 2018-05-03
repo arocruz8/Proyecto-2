7679efd48e24f18f3991b997ad0fdc6ec00a5778
Gilberto Rodriguez <Gilberto Rodriguez@192.168.1.9>
2018-05-03 17:59:42 -0600

    actualice la clase cola y nodo con librerias genercis

--- Progra2/src/estructurasDatos/Nodo.java
+++ Progra2/src/estructurasDatos/Nodo.java
@@ -1,5 +1,6 @@
 
 package estructurasDatos;
+/*
 public class Nodo {
     Object dato;
     Nodo next;
@@ -33,5 +34,34 @@ public class Nodo {
     public void setPrioridad(String prioridad){
         this.prioridad=prioridad;
     }
+}*/
+
+public class Nodo<T> {
     
+    private T dato;
+    private Nodo<T> siguiente;
+
+    public Nodo(){
+        siguiente=null;
+     }
+    public Nodo(T p){
+        siguiente=null;
+        dato = p;
+    }  
+    public Nodo(T t, Nodo<T> siguiente){
+        this.siguiente=siguiente;
+        dato = t;
+    }     
+    public T getDato() {
+        return dato;
+    } 
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }     
 }
--- Progra2/src/estructurasDatos/colaPrioridad.java
+++ Progra2/src/estructurasDatos/colaPrioridad.java
@@ -1,5 +1,7 @@
 package estructurasDatos;
+/*
 public class colaPrioridad {
+    /*
     Nodo inicio, fin;
     int tamaño;
     
@@ -42,7 +44,86 @@ public class colaPrioridad {
     //tamaño cola
     public int tamañoCola(){
         return this.tamaño;
-    }
+    }*/
+    
+    public class colaPrioridad<T>{
+	
+	private Nodo<T> inicio;
+	private static int size = 0;
+
+	public colaPrioridad(){
+	    inicio = null;
+	    size = 0;
+	}
+
+	public Nodo getTope(){
+		return inicio;
+	}
+
+	public static boolean esVacia(){
+	    return size ==0;
+	}
+	public static int getsize(){
+	    return size;
+	}
+
+	public Nodo<T> enqueue(T t){
+	    Nodo<T> nuevo = new Nodo<>(t);
+	    Nodo<T> aux;
+	    if (esVacia()) {
+	        inicio = nuevo;
+	    }else {
+	        aux = inicio;
+	
+	        while(aux.getSiguiente() != null){
+	            aux=aux.getSiguiente();
+	        }
+	        aux.setSiguiente(nuevo);
+	    }
+		System.out.println("Enqueue... Value:"+nuevo.getDato());
+	    size++;
+	    return nuevo;
+	}
+
+	public void dequeue(){
+		if (!esVacia()) {
+			System.out.println("Dequeue... Value:"+inicio.getDato());
+			inicio = inicio.getSiguiente();
+			size--;
+		}
+	}
+	
+	public void peek() throws Exception{		//public T peek() throws Exception{
+	    if(!esVacia()){
+	    	System.out.print("\nPeek... Value: "+inicio.getDato().toString()+"\n");
+	    } else {
+	        throw new Exception("> La pila se encuentra vacia.");
+	    }
+	}
+	
+	public boolean buscar(T referencia){
+	    Nodo<T> aux = inicio;
+	    boolean existe = false;
+	    while(existe != true && aux != null){
+	        if (referencia == aux.getDato()) {
+	            existe = true;
+	        }
+	        else{
+	            aux = aux.getSiguiente();
+	        }
+	    }
+	    return existe;
+	}
+	
+	public void imprimir(){
+	    Nodo<T> aux = inicio;
+		System.out.println("\nCola :");
+	    while(aux != null){
+	        System.out.println("¡_" + aux.getDato() + "_¡");
+	        aux = aux.getSiguiente();
+	    }
+		System.out.println("");
+	}
 }
    
     
