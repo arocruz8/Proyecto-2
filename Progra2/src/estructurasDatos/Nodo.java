90edbe98c43b4a8412015d7c015fb15cdfe05181
Gilberto Rodriguez <Gilberto Rodriguez@192.168.1.9>
2018-05-03 20:27:04 -0600

    agregue la variable prioridad en la clase nodo

--- Progra2/src/estructurasDatos/Nodo.java
+++ Progra2/src/estructurasDatos/Nodo.java
@@ -1,10 +1,12 @@
 
 package estructurasDatos;
 public class Nodo<cliente> {
-    
+    //atributos
     private cliente dato;
+    private Nodo<cliente> prioridad;
     private Nodo<cliente> siguiente;
-
+    
+    //constructores
     public Nodo(){
         siguiente=null;
      }
@@ -16,6 +18,8 @@ public class Nodo<cliente> {
         this.siguiente=siguiente;
         dato = t;
     }     
+    
+   //setter and getter
     public cliente getDato() {
         return dato;
     } 
@@ -27,5 +31,14 @@ public class Nodo<cliente> {
     }
     public void setSiguiente(Nodo<cliente> siguiente) {
         this.siguiente = siguiente;
-    }     
+    }
+    
+    public void setPrioridad(Nodo<cliente> prioridad){
+        this.prioridad=prioridad;
+    }
+    
+    public Nodo<cliente> getPrioriddad(){
+        return prioridad;
+    }
+         
 }
