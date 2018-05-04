f8422597a4b795bb07f9571335ebd89971cc63a8
Gilberto Rodriguez <Gilberto Rodriguez@192.168.1.9>
2018-05-03 18:28:25 -0600

    actualice la clase cola y nodo arregle las variables y comente codigo

--- Progra2/src/estructurasDatos/Nodo.java
+++ Progra2/src/estructurasDatos/Nodo.java
@@ -1,41 +1,4 @@
-
 package estructurasDatos;
-/*
-public class Nodo {
-    Object dato;
-    Nodo next;
-    String prioridad;
-    
-    public Nodo(Object dato){
-        this.dato=dato;
-        this.next=null;
-    }
-
-    public Object getDato() {
-        return dato;
-    }
-
-    public void setDato(Object dato) {
-        this.dato = dato;
-    }
-
-    public Nodo getNext() {
-        return next;
-    }
-
-    public void setNext(Nodo next) {
-        this.next = next;
-    }
-    
-    public String getPrioridad(){
-        return prioridad;
-    }
-    
-    public void setPrioridad(String prioridad){
-        this.prioridad=prioridad;
-    }
-}*/
-
 public class Nodo<cliente> {
     
     private cliente dato;
