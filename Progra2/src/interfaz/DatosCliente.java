5e5dcab8b1b19b6efd7c019a1e86a3d0b8c3bb3b
Gilberto Rodriguez <Gilberto Rodriguez@192.168.1.5>
2018-05-20 20:33:27 -0600

    daros clientes

--- Progra2/src/interfaz/DatosCliente.java
+++ Progra2/src/interfaz/DatosCliente.java
@@ -160,7 +160,6 @@ public class DatosCliente extends javax.swing.JFrame{
         System.out.println(h1.tamañoArreglo());
         System.out.println(h2.tamañoArreglo());
         colaSeguridad.imprimir();
-        h1.imprimir();
     }
     
     /*
